7ecfd2ce3373242fc0f8862e23b96ac06b39882b
Gustavo Barbosa <barbosa.gustavo@ifood.com.br>
2025-07-22 14:48:08 -0300

    adding hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.android.hilt)
+    alias(libs.plugins.google.ksp)
 }
 
 android {
@@ -57,4 +59,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Hilt dependencies
+    implementation(libs.android.hilt)
+    ksp(libs.android.hilt.compiler)
 }
