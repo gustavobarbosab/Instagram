c3fe3b4cc52c853c9b16d6b11e82bce9c24d1395
Gustavo Barbosa <gustavobarbosabarreto@gmail.com>
2025-10-15 11:42:18 -0300

    improving layout and logs

--- app/src/main/java/com/github/gustavobarbosab/instagram/feature/login/LoginContent.kt
+++ app/src/main/java/com/github/gustavobarbosab/instagram/feature/login/LoginContent.kt
@@ -17,6 +17,9 @@ import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -42,6 +45,7 @@ import com.github.gustavobarbosab.instagram.feature.login.components.SocialNetwo
 @Composable
 fun LoginContent(
     uiState: LoginUiState,
+    snackbarHostState: SnackbarHostState,
     onEmailChange: (String) -> Unit,
     onPasswordChange: (String) -> Unit,
     onLoginClick: () -> Unit,
@@ -50,103 +54,108 @@ fun LoginContent(
     onSignUpClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState())
-            .padding(MaterialTheme.sizing.size24),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.space16)
-    ) {
-        Spacer(modifier = Modifier.weight(1f))
+    Scaffold(
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+    ) { paddingValues ->
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .padding(paddingValues)
+                .padding(MaterialTheme.sizing.size24),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.space16)
+        ) {
+            Spacer(modifier = Modifier.weight(1f))
 
-        Text(
-            text = "Instagram",
-            fontFamily = FontFamily.Cursive,
-            style = MaterialTheme.typography.displayLarge.copy(
-                brush = MaterialTheme.customColors.gradient
+            Text(
+                text = "Instagram",
+                fontFamily = FontFamily.Cursive,
+                style = MaterialTheme.typography.displayLarge.copy(
+                    brush = MaterialTheme.customColors.gradient
+                )
             )
-        )
 
-        Spacer(modifier = Modifier.weight(.4f))
+            Spacer(modifier = Modifier.weight(.4f))
 
-        Card(modifier = Modifier.fillMaxWidth()) {
-            Column(
-                modifier = Modifier.padding(MaterialTheme.sizing.size24),
-                verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.space8)
-            ) {
-                InstagramTextField(
-                    value = uiState.email,
-                    onValueChange = onEmailChange,
-                    placeholder = stringResource(R.string.login_username_label),
-                    leadingIcon = {
-                        Icon(
-                            imageVector = Icons.Default.Email,
-                            contentDescription = stringResource(R.string.login_username_icon_description),
-                        )
-                    },
-                    keyboardType = KeyboardType.Email,
-                    errorMessage = uiState.emailError
-                )
+            Card(modifier = Modifier.fillMaxWidth()) {
+                Column(
+                    modifier = Modifier.padding(MaterialTheme.sizing.size24),
+                    verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.space8)
+                ) {
+                    InstagramTextField(
+                        value = uiState.email,
+                        onValueChange = onEmailChange,
+                        placeholder = stringResource(R.string.login_username_label),
+                        leadingIcon = {
+                            Icon(
+                                imageVector = Icons.Default.Email,
+                                contentDescription = stringResource(R.string.login_username_icon_description),
+                            )
+                        },
+                        keyboardType = KeyboardType.Email,
+                        errorMessage = uiState.emailError
+                    )
 
-                InstagramTextField(
-                    value = uiState.password,
-                    onValueChange = onPasswordChange,
-                    placeholder = stringResource(R.string.login_password_label),
-                    leadingIcon = {
-                        Icon(
-                            imageVector = Icons.Default.Lock,
-                            contentDescription = stringResource(R.string.login_password_label),
-                        )
-                    },
-                    trailingIcon = { toggle ->
-                        Icon(
-                            imageVector = if (toggle) {
-                                Icons.Default.CheckCircle
-                            } else {
-                                Icons.Default.AddCircle
-                            },
-                            contentDescription = if (toggle) {
-                                stringResource(R.string.login_password_hidden)
-                            } else stringResource(
-                                R.string.login_password_visible
-                            ),
-                        )
-                    },
-                    defaultVisualTransformation = PasswordVisualTransformation(),
-                    imeAction = ImeAction.Done,
-                    errorMessage = uiState.passwordError
-                )
+                    InstagramTextField(
+                        value = uiState.password,
+                        onValueChange = onPasswordChange,
+                        placeholder = stringResource(R.string.login_password_label),
+                        leadingIcon = {
+                            Icon(
+                                imageVector = Icons.Default.Lock,
+                                contentDescription = stringResource(R.string.login_password_label),
+                            )
+                        },
+                        trailingIcon = { toggle ->
+                            Icon(
+                                imageVector = if (toggle) {
+                                    Icons.Default.CheckCircle
+                                } else {
+                                    Icons.Default.AddCircle
+                                },
+                                contentDescription = if (toggle) {
+                                    stringResource(R.string.login_password_hidden)
+                                } else stringResource(
+                                    R.string.login_password_visible
+                                ),
+                            )
+                        },
+                        defaultVisualTransformation = PasswordVisualTransformation(),
+                        imeAction = ImeAction.Done,
+                        errorMessage = uiState.passwordError
+                    )
 
-                PrimaryButton(
-                    onClick = onLoginClick,
-                    isLoading = uiState.isLoading,
-                    text = stringResource(R.string.login_button),
-                    modifier = Modifier.fillMaxWidth(),
-                )
+                    PrimaryButton(
+                        onClick = onLoginClick,
+                        isLoading = uiState.isLoading,
+                        text = stringResource(R.string.login_button),
+                        modifier = Modifier.fillMaxWidth(),
+                    )
 
-                TertiaryButton(
-                    onClick = onForgotPasswordClick,
-                    text = stringResource(R.string.login_forgot_password),
-                    modifier = Modifier.align(Alignment.CenterHorizontally)
-                )
+                    TertiaryButton(
+                        onClick = onForgotPasswordClick,
+                        text = stringResource(R.string.login_forgot_password),
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    )
 
-                DividerWithText("OR")
+                    DividerWithText("OR")
 
-                SocialNetworkLoginButton(
-                    text = stringResource(R.string.login_facebook_method),
-                    onClick = onFacebookLoginClick
-                )
+                    SocialNetworkLoginButton(
+                        text = stringResource(R.string.login_facebook_method),
+                        onClick = onFacebookLoginClick
+                    )
+                }
             }
+
+            Spacer(modifier = Modifier.height(MaterialTheme.spacing.space16))
+
+            SignUpPrompt(
+                questionLabel = stringResource(R.string.login_sign_up_question),
+                signUpLabel = stringResource(R.string.login_sign_up),
+                onSignUpClick = onSignUpClick
+            )
         }
-
-        Spacer(modifier = Modifier.height(MaterialTheme.spacing.space16))
-
-        SignUpPrompt(
-            questionLabel = stringResource(R.string.login_sign_up_question),
-            signUpLabel = stringResource(R.string.login_sign_up),
-            onSignUpClick = onSignUpClick
-        )
     }
 }
 
@@ -172,6 +181,7 @@ private fun Preview() {
             onFacebookLoginClick = { },
             onForgotPasswordClick = { },
             onSignUpClick = { },
+            snackbarHostState = SnackbarHostState()
         )
     }
 }
--- app/src/main/java/com/github/gustavobarbosab/instagram/feature/login/LoginScreen.kt
+++ app/src/main/java/com/github/gustavobarbosab/instagram/feature/login/LoginScreen.kt
@@ -1,16 +1,13 @@
 package com.github.gustavobarbosab.instagram.feature.login
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import android.util.Log
 import androidx.compose.material3.SnackbarDuration
-import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -52,28 +49,25 @@ fun LoginScreen(
 
     LaunchedEffect(uiState.isLoginSuccessful) {
         if (uiState.isLoginSuccessful) {
-            delay(3000)
+            Log.d("Teste", "esperando 4 segundos antes de navegar")
+            delay(4000)
             navController.popBackStack()
             navController.navigate(HomeRoute)
             viewModel.loginSuccessfulHandled()
         }
     }
 
-    Scaffold(
-        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
-    ) { paddingValues ->
-        LoginContent(
-            modifier = Modifier.padding(paddingValues),
-            uiState = uiState,
-            onEmailChange = viewModel::updateEmail,
-            onPasswordChange = viewModel::updatePassword,
-            onLoginClick = {
-                keyboardController?.hide()
-                viewModel.login()
-            },
-            onFacebookLoginClick = viewModel::loginWithFacebook,
-            onForgotPasswordClick = viewModel::forgotPassword,
-            onSignUpClick = onNavigateToSignUp,
-        )
-    }
+    LoginContent(
+        uiState = uiState,
+        snackbarHostState = snackbarHostState,
+        onEmailChange = viewModel::updateEmail,
+        onPasswordChange = viewModel::updatePassword,
+        onLoginClick = {
+            keyboardController?.hide()
+            viewModel.login()
+        },
+        onFacebookLoginClick = viewModel::loginWithFacebook,
+        onForgotPasswordClick = viewModel::forgotPassword,
+        onSignUpClick = onNavigateToSignUp,
+    )
 }
